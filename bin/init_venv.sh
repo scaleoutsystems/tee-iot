1c8a6a7d42f5bf9ede2600caffa211182da64dbb
mcapuccini <marco@scaleoutsystems.com>
2022-05-13 12:19:25 +0000

    entrypoint working

--- bin/init_venv.sh
+++ bin/init_venv.sh
@@ -7,3 +7,4 @@ python -m venv .venv
 # Pip deps
 .venv/bin/pip install --upgrade pip
 .venv/bin/pip install -r requirements.txt
+.venv/bin/pip install -e "git+https://github.com/scaleoutsystems/fedn.git@develop#egg=fedn&subdirectory=fedn"
